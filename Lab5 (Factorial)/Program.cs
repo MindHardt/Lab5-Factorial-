6e64f40826a6ab488a329af541cc45eab1185c75
MindHardt <71509337+MindHardt@users.noreply.github.com>
2020-10-14 18:19:56 +0500

    Initial commit

---
+++ Lab5 (Factorial)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5__Factorial_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
