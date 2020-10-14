1d899957a7d8c7e71a75ff7bc22d59b2ac77188c
MindHardt <71509337+MindHardt@users.noreply.github.com>
2020-10-14 19:11:54 +0500

    Done v1.0
    Done

--- Lab5 (Factorial)/Program.cs
+++ Lab5 (Factorial)/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Threading;
 
 namespace Lab5__Factorial_
 {
@@ -6,7 +8,55 @@ namespace Lab5__Factorial_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BuildFrame(CalcFactorial(GetData()));
+        }
+        static long CalcFactorial(long n)
+        {
+            if (n == 0) return 1;
+            return n * CalcFactorial(n - 1);
+        }
+        static void BuildFrame(long n)
+        {
+            bool color = true;
+            int width = (n.ToString()).Length;
+            while (1 != 0)
+            {
+                if (color == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else Console.ResetColor();
+                Console.Clear();
+                BuildLine(width);
+                Console.WriteLine($"||{n}||");
+                BuildLine(width);
+                Thread.Sleep(500);
+                color = !color;
+            }
+        }
+        static void BuildLine(int n)
+        {
+            Console.Write("||");
+            for (int i = 0; i < n; i++) Console.Write("=");
+            Console.WriteLine("||");
+        }
+
+        static int GetData()
+        {
+            bool err = false;
+            int answerint;
+            do
+            {
+                Console.WriteLine("Введите число, от которого нужно получить факториал.");
+                string answerstring = Console.ReadLine();
+                if (!int.TryParse(answerstring, out answerint)) err = true;
+                if (err == true) Console.WriteLine("Введено недопустимое значение.");
+            }
+            while (err == true);
+            return (answerint);
+
         }
     }
+
 }
